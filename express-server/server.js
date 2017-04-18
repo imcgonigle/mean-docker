6a22f219c050cc6aa7139cbb516142c0256c9df5
Ian McGonigle <ian.is.mcgonigle@gmail.com>
2017-04-18 14:35:11 -0600

    add cross origin middleware

--- express-server/server.js
+++ express-server/server.js
@@ -13,6 +13,13 @@ const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
+// Cross Origin middleware
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*")
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
+    next()
+})
+
 // Set our api routes
 app.use('/', api);
 
